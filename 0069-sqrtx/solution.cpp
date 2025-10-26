5c164a63b0317d29dbc470c7e54797578ce2ebed
Manveer Makkar <makkarmanveer@gmail.com>
2025-10-26 08:31:56 +0000

    [LeetCode Sync] Runtime - 15 ms (9.96%), Memory - 8.7 MB (14.09%)

---
+++ 0069-sqrtx/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0)
+        return 0;
+        long long i;
+        for(i=1;i*i<=x;i++){
+            if(i*i==x)
+                return i;
+        }
+        return i-1;
+            }
+};
