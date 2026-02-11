03ddd18ea64370d55e013572bfdd7099e47fc2dc
Manveer Makkar <makkarmanveer@gmail.com>
2026-02-11 14:27:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (43.28%)

---
+++ 0050-powx-n/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    double solve(double x, long long n){
+        if(n == 0) return 1;
+        double y = solve(x, n/2);
+        if(n % 2 == 0){
+            return y * y;
+        }
+        else{
+            return x * y * y;
+        }
+    }
+    
+    double myPow(double x, int n) {
+        long long N = n;   
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+        return solve(x, N);
+    }
+};
+
