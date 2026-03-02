8686888370c3abe954b41b268bdbb3ff276add58
Manveer Makkar <makkarmanveer@gmail.com>
2026-03-02 07:52:56 +0000

    [LeetCode Sync] Runtime - 792 ms (5.10%), Memory - 35.6 MB (96.94%)

---
+++ 0724-find-pivot-index/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    int pivotIndex(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            int lsum=0,rsum=0;
+           for(int j=0;j<i;j++){
+              lsum+=nums[j];
+           }
+           for(int k=i+1;k<nums.size();k++){
+            rsum+=nums[k];
+           }
+           if(lsum==rsum) return i;
+        }
+        return -1;
+        }
+};
