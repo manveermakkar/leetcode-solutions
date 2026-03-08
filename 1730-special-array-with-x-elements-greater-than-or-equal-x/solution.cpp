fddcd59752d5d973ce124561cba8d171290568b6
Manveer Makkar <makkarmanveer@gmail.com>
2026-03-08 08:21:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.1 MB (64.91%)

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        for(int i=1;i<=nums.size();i++){
+            int count=0;
+            for(int j=0;j<nums.size();j++){
+                if(i<=nums[j]) count++;
+            }
+            if(count==i) return i;
+        }
+        return -1;
+    }
+};
