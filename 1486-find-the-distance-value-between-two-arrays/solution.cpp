84d3eee04efd908cde65537ceac2b728ae56d52f
Manveer Makkar <makkarmanveer@gmail.com>
2026-03-09 14:11:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.6 MB (88.04%)

---
+++ 1486-find-the-distance-value-between-two-arrays/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int count=0;
+        for(int i=0;i<arr1.size();i++){
+            int c=0;
+            for(int  j=0;j<arr2.size();j++){
+                if(abs(arr1[i]-arr2[j])<=d) break;
+                else c++;
+            }
+            if(c==arr2.size())count++;
+        }
+        return count;
+    }
+};
