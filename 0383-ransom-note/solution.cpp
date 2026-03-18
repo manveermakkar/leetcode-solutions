db1aeb852fcf29b6b94f43fc7c5d48a280c322d4
Manveer Makkar <makkarmanveer@gmail.com>
2026-03-18 06:23:21 +0000

    [LeetCode Sync] Runtime - 16 ms (5.85%), Memory - 11.6 MB (89.46%)

---
+++ 0383-ransom-note/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count=0;
+        for(int i=0;i<ransomNote.size();i++){
+            for(int j=0;j<magazine.size();j++){
+                if(ransomNote[i]==magazine[j]) {
+                    magazine.erase(magazine.begin()+j);
+                    count++;
+                    break;
+            }
+        }
+    }
+    if(count==ransomNote.size()) return true;
+    else return false;
+    }
+};
