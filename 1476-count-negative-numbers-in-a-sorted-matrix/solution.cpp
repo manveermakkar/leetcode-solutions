b37288c28b85716e27fe17b6ea29a3de1b329479
Manveer Makkar <makkarmanveer@gmail.com>
2026-03-31 07:21:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14 MB (92.66%)

---
+++ 1476-count-negative-numbers-in-a-sorted-matrix/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int c=0;
+        for(int i=grid.size()-1;i>=0;i--){
+            for(int j=grid[i].size()-1;j>=0;j--){
+                  if(grid[i][j]<0) c++;
+                  else break;
+            }
+        }
+        return c;
+    }
+};
