df21e68627bdb709341d4ac94ceda64be256a8f0
Manveer Makkar <makkarmanveer@gmail.com>
2026-04-04 11:34:57 +0000

    [LeetCode Sync] Runtime - 3 ms (17.28%), Memory - 26.3 MB (97.36%)

---
+++ 2892-check-if-array-is-good/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        if (nums[n-1] != n-1) return false;
+        if (nums[n-1] != nums[n-2]) return false;
+        for (int i = 0; i < n-2; i++) {
+            if (nums[i] != i+1) return false;
+        }
+        return true;
+    }
+};
