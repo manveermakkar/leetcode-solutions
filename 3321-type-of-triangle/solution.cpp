346e45dbf0b8c8d390edbebfb15c81beb9642d6b
Manveer Makkar <makkarmanveer@gmail.com>
2026-04-12 14:37:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23 MB (37.27%)

---
+++ 3321-type-of-triangle/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if((nums[0]+nums[1]<=nums[2]) || nums[1]+nums[2]<=nums[0] || nums[0]+nums[2]<=nums[1]) return "none";
+        if(nums[0]==nums[1]  && nums[2]==nums[1]) return "equilateral";
+        if(nums[0]==nums[1] && nums[1]!=nums[2]) return "isosceles";
+        if(nums[0]==nums[2] && nums[1]!=nums[2]) return "isosceles";
+        if(nums[1]==nums[2] && nums[1]!=nums[0]) return "isosceles";
+        return "scalene";
+    }
+};
