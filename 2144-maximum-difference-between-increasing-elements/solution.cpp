db4c55756c4d6a70b8d335f3020735d8f4a3f516
Manveer Makkar <makkarmanveer@gmail.com>
2026-04-23 14:04:37 +0000

    [LeetCode Sync] Runtime - 7 ms (10.47%), Memory - 11.9 MB (66.64%)

---
+++ 2144-maximum-difference-between-increasing-elements/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int diff=-1;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]<nums[j]) diff=max(diff,nums[j]-nums[i]);
+            }
+        }
+        return diff;
+    }
+};
