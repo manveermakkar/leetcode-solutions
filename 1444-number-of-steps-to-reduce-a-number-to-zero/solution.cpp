06db955ebc7f3c2a37633bf7c4e70319925b7bba
Manveer Makkar <makkarmanveer@gmail.com>
2026-05-08 17:54:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (80.11%)

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int s=0;
+        while(num>0){
+            if(num%2==0) {
+                num=num/2;
+                s++;
+        }
+        else{
+            num--;
+            s++;
+        }
+       
+    }
+     return s;
+    }
+};
